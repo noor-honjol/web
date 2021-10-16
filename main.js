7a73da38c0af12fc7eb92cabca1611ff9fbb2cf4
noor-honjol <noorhonjol09@gmail.com>
2021-10-16 22:43:36 +0300

    aa

---
+++ main.js
@@ -0,0 +1,118 @@
+//alert('yooo');
+/*
+console.log('aaa');
+var b= 'aaaa';
+var f=45;
+console.log(b);
+console.log(f);
+*/
+
+//var age=prompt('what is your age?');
+//document.getElementById('sometext').innerHTML=age;
+//console.log(age);
+
+
+//function 
+/*
+function fun(){
+    alert('afasf');
+    
+}
+fun();
+function aa(){
+    var name=prompt('sss');
+    console.log('hello '+name);
+}
+aa();
+function sum(a1,a2){
+    var result =a1+a2;
+    console.log(result);
+}
+sum(1,2);
+*/
+
+
+//loops 
+//while loop
+
+/*
+var num=0;
+while (num<100){
+    num++;
+}
+console.log(num);
+//test 
+var num=567;
+while(num>0){
+    console.log(num%10);
+    num/=10;
+    num=Math.floor(num);
+}
+*/
+
+//for loop 
+
+/*
+for (i=0 ;i<100;i++){
+    console.log(i);
+}
+let a=['a',1,3,1.5];
+console.log(a);
+
+let y={'a':55};
+console.log(y['a']); 
+*/
+
+//string 
+
+/*
+let fruit='banana';
+console.log(fruit.length);
+console.log(fruit.indexOf('nan'));
+console.log(fruit.slice(0,6));
+
+console.log(fruit.split(''))//aa
+*/
+
+//array
+
+/*
+let fruit=['banana','apple','orange','pineapples'];
+console.log(fruit);
+
+console.log(fruit[3])
+console.log(fruit.join('*'));
+
+let lala=new Array();
+for(let i=0;i<5;i++){
+    lala.push(prompt());
+}
+console.log(lala);
+
+
+let lla=new Array();
+for(let i=0;i<5;i++){
+    lla.push(i);
+}
+console.log(lla);
+*/
+
+//object
+
+/*
+let student = {first:'rafeh',last:'qazi',age:150,height:170};
+
+console.log(student.first);
+console.log(student.last);
+console.log(student);
+*/
+
+// conditianal statment
+a=prompt("what your age");
+
+if (a>18&&a<35 ){
+    console.log('ddd');
+}
+else{
+    console.log('ffgg')
+}
